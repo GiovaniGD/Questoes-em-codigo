26648cb5ad76ef057e9943fd6897c7e150989bab
GiovaniGD <100315032+GiovaniGD@users.noreply.github.com>
2023-03-08 18:39:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// 6)
+
+class Ator{
+    constructor(nome, nacionalidade){
+        this.nome=nome;
+        this.nacionalidade=nacionalidade;
+    }
+}
+
+// 7)
+
+class Diretor{
+    constructor(nome, nacionalidade){
+        this.nome=nome;
+        this.nacionalidade=nacionalidade;
+    }
+}
+
+//8)
+
+class Filme{
+    constructor(id, titulo, ano, genero, duracao, sinopse, cartaz, direcao, elenco, classificacao, avaliacao){
+        this.id=id;
+        this.titulo=titulo;
+        this.ano=ano;
+        this.genero=genero;
+        this.duracao=duracao;
+        this.sinopse=sinopse;
+        this.cartaz=cartaz;
+        this.direcao=direcao;
+        this.elenco=elenco;
+        this.classificacao=classificacao;
+        this.avaliacao=avaliacao;
+    }
+}
